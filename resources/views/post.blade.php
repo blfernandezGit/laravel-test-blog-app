1958e2ff73e719759d5519320b0b4541015db2eb
blfernandezGit <blfernandez@up.edu.ph>
2022-02-20 17:47:24 +0800

    add post variable

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,7 @@
 <link rel="stylesheet" href="/app.css">
 
 <article>
-    <h1><a href="/post">My First Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quo officia reprehenderit nulla totam vitae beatae expedita sit sunt optio alias, delectus quas nisi. Vitae aut eius impedit doloribus ipsam!
-    </p>
+    <?= $post; ?>
 </article>
 
 <a href="/">Go Back</a>
