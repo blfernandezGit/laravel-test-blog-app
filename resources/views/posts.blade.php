03ea6e4306a32c675f23414f199c95128cf47a79
blfernandezGit <blfernandez@up.edu.ph>
2022-02-20 18:18:26 +0800

    add other posts in homepage

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,4 +10,18 @@
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quo officia reprehenderit nulla totam vitae beatae expedita sit sunt optio alias, delectus quas nisi. Vitae aut eius impedit doloribus ipsam!
         </p>
     </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quo officia reprehenderit nulla totam vitae beatae expedita sit sunt optio alias, delectus quas nisi. Vitae aut eius impedit doloribus ipsam!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quo officia reprehenderit nulla totam vitae beatae expedita sit sunt optio alias, delectus quas nisi. Vitae aut eius impedit doloribus ipsam!
+        </p>
+    </article>
 </body>
