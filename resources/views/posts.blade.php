07c7fa70574b2f92f29c1d7cd59790f3f9ee62e5
blfernandezGit <blfernandez@up.edu.ph>
2022-02-20 17:43:29 +0800

    base css + html with post route

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quo officia reprehenderit nulla totam vitae beatae expedita sit sunt optio alias, delectus quas nisi. Vitae aut eius impedit doloribus ipsam!
+        </p>
+    </article>
+</body>
